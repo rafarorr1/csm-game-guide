8dba99b418c83546024683cf56e1b3113efe1d4a
rafarorr1 <rafarorr1@gmail.com>
2026-09-09 18:39:30 -0600

    Publica la beta validada de aaaef96 en Cloudflare
    Preview aislado; conserva gh-pages/tcg y main.

--- tcg/sw.js
+++ tcg/sw.js
@@ -18,7 +18,7 @@
    ========================================================================== */
 'use strict';
 
-const VERSION = 227;
+const VERSION = 228;
 const PREFIJO = 'caoz-cache-' + new URL(self.registration.scope).pathname + '-';
 const CACHE = PREFIJO + VERSION;
 const NUCLEO = ['./', 'index.html', 'movil.html', 'motor.js', 'final.js', 'final-core.js', 'campana-mesa.js', 'campana-personaje.js', 'campana-deseo.js', 'campana-pitagoras.js', 'campana-secreto.js', 'campana-honores.js', 'pitagoras-pruebas.js', 'pitagoras-combate.js', 'pitagoras-mesa.js', 'pitagoras-mundos.js', 'pitagoras-cine.js', 'pitagoras-laboratorio.js', 'pitagoras-fps.js', 'pitagoras-pixel.js', 'dado-fisico.js', 'polish-aaa.js', 'audio-domo.js', 'audio/catalogo.json', 'manifest.webmanifest',
